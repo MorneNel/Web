95a0be98ff2480c971d9261cebd4da86130db8b6
Roy Wenman <roy.wenman@mela.co.uk>
2013-08-27 13:26:03 +0100

    Physiology was not passing numbers with periods
    Value needed to be changed as a number like 25.6 would be passed to 4D
    as 256 which would kick up an unnecessary warning

--- Outreach/checkPhysiology.php
+++ Outreach/checkPhysiology.php
@@ -11,7 +11,7 @@ if (!$_REQUEST['label']) die("No field label specified");
 $dlkID = filter_var($_REQUEST['dlkid'], FILTER_SANITIZE_NUMBER_INT);
 $code = filter_var($_REQUEST['code'], FILTER_SANITIZE_STRING);
 $type = filter_var($_REQUEST['type'], FILTER_SANITIZE_STRING);
-$value = filter_var($_REQUEST['value'], FILTER_SANITIZE_NUMBER_FLOAT);
+$value = $_REQUEST['value'];//filter_var($_REQUEST['value'], FILTER_SANITIZE_NUMBER_FLOAT); // Thought FLOAT sanitisation would be fine but it removes periods for some reason
 $label = filter_var($_REQUEST['label'], FILTER_SANITIZE_STRING);
 
 echo $Mela_SQL->Exec4DSQL("SQL_PhysioValid", "$dlkID,'$code','$type',$value,'$label'"); 
